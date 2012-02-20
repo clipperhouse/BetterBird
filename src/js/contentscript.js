ee370dd5765e11e7af09a5fac745ea6cd126e867
Matt Sherman <mwsherman@gmail.com>
2012-02-19 21:10:21 -0500

    first implementation of options UI

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,6 +1,31 @@
+(function($) {
+  $.fn.filterByData = function(key, value) {
+  	return this.filter(function () {
+		return $(this).data(key) == value;
+	});
+  };
+  $.fn.findByData = function(selector, key, value) {
+  	return this.find(selector).filterByData(key, value);
+  };
+  $.fn.id = function(id) {
+  	return this.attr("id", id);
+  };
+  $.fn.for = function(cb) {
+  	return this.attr("for", cb.attr("id"));
+  };
+  $.fn.checked = function(ischecked) {
+  	if (ischecked === true) {
+  		return this.attr("checked", "checked");
+  	}
+  	if (ischecked === false) {
+  		return this.removeAttr("checked", "checked");
+  	}
+  };
+})(jQuery);
+
+String.prototype.remove = function(r) { return this.replace(r, ''); };
 
 (function() {
-	String.prototype.remove = function(r) { return this.replace(r, ''); };
 
 	var BetterBird = (function () {
 		var regex = {
@@ -10,19 +35,24 @@
 			fileext: /(.html|.php|.aspx)/i,
 			querystring: /\?.*$/,
 		};
+		
 		var classnames = { 
 			expand: "bb-expand", 
 			direct: "bb-direct", 
-			savedsearches: "bb-saved-search"
+			savedsearch: "bb-saved-search",
+			options: "bb-options",
+			birdblock: "bb-birdblock",
 		};
+
 		var body = $(document.body);
 		var wrapper = $("div.wrapper");
 		var dashboard = $("div.dashboard", wrapper);
+		var birdBlock, searchModule, optionsModule;
 
 		var applyCss = function (options) {
 			for (var key in options.styles) {
-				var classname = "bb-" + key;
-				body.toggleClass(classname, options.styles[key]);
+				classnames[key] = "bb-" + key;
+				body.toggleClass(classnames[key], options.styles[key]);
 			}
 			wrapper.show();
 		};
@@ -50,25 +80,30 @@
 			});
 		};
 
-		var getSavedSearches = function () {
-			var module = (function () {
-				var existing = $("div.module." + classnames.savedsearch).find("div.flex-module");
-				if (existing.length) {
-					return existing;
-				}
-				var f = $("<div class='flex-module' />").append($("<h3 />").css("margin-bottom", "10px").text("Saved Searches"));
-				var m = $("<div class='module' />").addClass(classnames.savedsearch).css("background-color", "#fff").append(f);
-				$("div.mini-profile", dashboard).after(m);
-				return f;
-			})();
+		var createModule = function(classname, title, iconurl) {
+			var h = $("<h3 />").css("margin-bottom", "10px").text(title);
+			if (iconurl) {
+				h.prepend($("<img>").addClass("bb-icon").attr("src", iconurl));
+			}
+			var f = $("<div class='flex-module' />").append(h);
+			var m = $("<div class='module' />").addClass(classname).css("background-color", "#fff").append(f);
+			m.content = f;
+			m.title = h;
+			return m;
+		};
 
-			module.find("p").remove();
+		var getSavedSearches = function () {
+			if (searchModule === undefined) {
+				searchModule = createModule(classnames.savedsearch, "Saved Searches", chrome.extension.getURL("img/twitter_32.png"));
+				birdBlock.append(searchModule);
+			}
 
 			var updateSavedSearch = function (q, count) {
-				var a = module.find("a[data-search-query='" + q + "']");
+				var datakey = "search-query";
+				var a = searchModule.content.findByData("a", datakey, q);
 				if (a.length == 0) {
-					a = $("<a>").data("search-query", q).attr("href", "#!/search/" + encodeURIComponent(q));
-					module.append($("<p>").append(a));
+					a = $("<a>").data(datakey, q).attr("href", "#!/search/" + encodeURIComponent(q));
+					searchModule.content.append($("<p>").append(a));
 				}
 				a.text(q + " (" + count + ")");
 			};
@@ -100,7 +135,41 @@
 			$("a.twitter-atreply").removeClass("twitter-atreply pretty-link");
 		};
 
+		var createOptionsModule = function() {
+			optionsModule = createModule(classnames.options, "Better Bird settings", chrome.extension.getURL("img/twitter_32.png"));
+
+			addStyleOptionCheckbox("columnswitch", "Switch columns");
+			addStyleOptionCheckbox("columnwide", "Widen content");
+
+			birdBlock.append(optionsModule);
+		};
+
+		var addStyleOptionCheckbox = function(optionkey, labeltext, iconurl) {
+			var cb = $("<input type='checkbox'>")
+					 .id("bb-option-" + optionkey).checked(options.styles[optionkey])
+					 .change(function() {
+					 	options.styles[optionkey] = $(this).is(":checked");
+						body.toggleClass(classnames[optionkey], options.styles[optionkey]);
+						saveOptions();
+					 });
+			var label = $("<label>").text(labeltext).for(cb);
+			var br = $("<br>")
+			
+			optionsModule.content.append(cb).append(label).append(br);
+		};
+
+		var createBirdBlock = function() {
+			birdBlock = $("<div>").addClass(classnames.birdblock);
+			$("div.mini-profile", dashboard).after(birdBlock);
+		};
+
 		var options;
+		var saveOptions = function() {
+			chrome.extension.sendRequest({ type: "save-options", options: options }, function() {
+				console.log("Saved options");
+			});
+		};
+
 		var urlinterval, searchinterval;
 
 		return {
@@ -112,7 +181,8 @@
 					options = response;
 					applyCss(options);
 					setTimeout(function() {					// a little time for the ajax to populate
-						$("div.trends").hide();
+						createBirdBlock();
+						
 						urlinterval = setInterval(function() {
 							var stream = $("div.stream");
 							if (options.expandurls) {
@@ -131,6 +201,7 @@
 								}, 60 * 60 * 1000 * numsearches / 150);
 							}
 						}
+						createOptionsModule();
 					}, 1500);
 				});
 			}			
